55171a302cfbfba17ebf5dc890a31e60b4c7d80d
Gregory Beaver <greg.beaver.cello@gmail.com>
2021-01-01 21:40:05 -0500

    fix types in Link (#4)

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,37 +1,46 @@
 import React from 'react';
 import { MakeProps, getClassNameProps } from './BaseComponent';
-import useReveal from "../hooks/useReveal";
-import Reveal from "reveal.js";
+import useReveal from '../hooks/useReveal';
+import Reveal from 'reveal.js';
+
+export type SlideType = string | number | [number, number] | HTMLElement;
 
 export interface LinkProps {
   children: React.ReactNode;
   href?: string;
-  slide?: string | number | [number, number] | HTMLElement;
+  slide?: SlideType;
 }
 
 function getLinkFromSlide(reveal: Reveal | null, slide: HTMLElement) {
   if (!reveal) {
-    return '#'
+    return '#';
   }
   const indices = reveal.getIndices(slide);
   return (e: React.MouseEvent) => {
     e.preventDefault();
     e.stopPropagation();
     reveal.slide(indices.h, indices.v, indices.f);
-  }
+  };
 }
 
-function getLink(reveal: Reveal | null, href?: string, slide?: string | [string, string] | HTMLElement): string | ((e: React.MouseEvent) => void) {
+function getLink(
+  reveal: Reveal | null,
+  href?: string,
+  slide?: SlideType,
+): string | ((e: React.MouseEvent) => void) {
   if (href) {
     return href;
   }
-  if (typeof slide === "string") {
+  if (!slide) {
+    return '#';
+  }
+  if (typeof slide === 'string') {
     // slide is an id
     const slideById = document.querySelector<HTMLElement>(`#${slide}`);
     if (slideById) {
       return getLinkFromSlide(reveal, slideById);
     }
-  } else if (typeof slide === "number") {
+  } else if (typeof slide === 'number') {
     return `#${slide - 1}`;
   } else if (Array.isArray(slide)) {
     return `#${slide[0] - 1}/${slide[1] - 1}`;
@@ -51,7 +60,8 @@ function Link({
 }: MakeProps<LinkProps, 'a'>) {
   const { reveal } = useReveal();
   const onClickOrHref = getLink(reveal, href, slide);
-  const onClick = typeof onClickOrHref === 'function' ? onClickOrHref : undefined;
+  const onClick =
+    typeof onClickOrHref === 'function' ? onClickOrHref : undefined;
   const link = typeof onClickOrHref === 'string' ? onClickOrHref : undefined;
   return (
     <a
