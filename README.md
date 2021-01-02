d66c352336d2d225d764f239cc90df116d747ae2
Gregory Beaver <greg.beaver.cello@gmail.com>
2021-01-01 21:32:59 -0500

    Improve v0.3 (#3)
    * prepare for v0.3, fix the API for Link
    
    * fix the docs for Link

--- README.md
+++ README.md
@@ -492,6 +492,12 @@ supports all the standard `li` parameters plus the reveal.js addons above
 
 #### Link
 
+supports all the standard `a` parameters plus the reveal.js addons above. In addition,
+the `Link` component makes it possible to link to slides easily.
+Pass in the slide number (1-indexed), the slide id (a string), or the slide itself,
+retrieved using a ref, and this will link to the slide properly. Preferred is using the
+slide ID or the slide itself, as this will survive API changes in reveal.js
+
 
 #### Main
 
--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import { MakeProps, getClassNameProps } from './BaseComponent';
+import useReveal from "../hooks/useReveal";
+import Reveal from "reveal.js";
 
 export interface LinkProps {
   children: React.ReactNode;
   href?: string;
-  slide?: string | [string, string];
+  slide?: string | number | [number, number] | HTMLElement;
+}
+
+function getLinkFromSlide(reveal: Reveal | null, slide: HTMLElement) {
+  if (!reveal) {
+    return '#'
+  }
+  const indices = reveal.getIndices(slide);
+  return (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    reveal.slide(indices.h, indices.v, indices.f);
+  }
+}
+
+function getLink(reveal: Reveal | null, href?: string, slide?: string | [string, string] | HTMLElement): string | ((e: React.MouseEvent) => void) {
+  if (href) {
+    return href;
+  }
+  if (typeof slide === "string") {
+    // slide is an id
+    const slideById = document.querySelector<HTMLElement>(`#${slide}`);
+    if (slideById) {
+      return getLinkFromSlide(reveal, slideById);
+    }
+  } else if (typeof slide === "number") {
+    return `#${slide - 1}`;
+  } else if (Array.isArray(slide)) {
+    return `#${slide[0] - 1}/${slide[1] - 1}`;
+  } else if (slide) {
+    return getLinkFromSlide(reveal, slide);
+  }
+  return '#';
 }
 
 function Link({
@@ -15,20 +49,17 @@ function Link({
   slide,
   ...props
 }: MakeProps<LinkProps, 'a'>) {
+  const { reveal } = useReveal();
+  const onClickOrHref = getLink(reveal, href, slide);
+  const onClick = typeof onClickOrHref === 'function' ? onClickOrHref : undefined;
+  const link = typeof onClickOrHref === 'string' ? onClickOrHref : undefined;
   return (
     <a
       {...getClassNameProps(props)}
       data-id={id}
       id={id}
-      href={
-        href ||
-        (slide &&
-          `#/${
-            typeof slide === 'string'
-              ? parseInt(slide, 10) - 1
-              : `${parseInt(slide[0], 10) - 1}/${parseInt(slide[1], 10) - 1}`
-          }`)
-      }
+      href={link}
+      onClick={onClick}
       data-fragment-index={fragmentIndex}
     >
       {children}
